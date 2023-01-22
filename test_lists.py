987fe36fd07e25d3c81b2ca22af8b8909c56000b
LevinOX <daniel.wrede@posteo.de>
2023-01-22 20:17:35 +0100

    building on list structure

---
+++ test_lists.py
@@ -0,0 +1,11 @@
+job_data = [[None, None, []] for i in range(3)]
+job_data[0][0] = 'URL'
+job_data[0][1] = 'job_title'
+# job_data[0].append([])
+job_data[0][2][0] = 'competence1'
+# job_data[0][2].append('competence2')
+# job_data[0][2].append('competence3')
+
+job_data[1][0] = 'URL2'
+
+print(job_data)
