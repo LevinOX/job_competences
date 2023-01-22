8a8514fdf76f8f050dc6c7e3b305fda53ff708ae
LevinOX <daniel.wrede@posteo.de>
2023-01-22 21:01:34 +0100

    adding 'new_words' as list.

--- filter_job_competences.py
+++ filter_job_competences.py
@@ -18,7 +18,7 @@ def filter_string(string):
 with open("job_description.txt", "r", encoding='utf-8') as f:
     jdescription = filter_string(f.read(500))
     jdes = tuple(jdescription.split(sep=' '))
-with open("usual_words_de_big.csv", "r") as g:
+with open("usual_words_de.csv", "r") as g:
     usual_words = list(csv.reader(g, delimiter=','))[0]
     # usual_words = g.read()  # <-- list
 with open("competences.csv", "r") as h:
@@ -28,6 +28,19 @@ with open("competences.csv", "r") as h:
 print(f"list of competences: \n{competences}")
 print(f"job description: {jdescription}")
 print(usual_words[:20])
+
+# job_data-structure: [['URL', 'job_title', ['comp1', 'comp2', 'comp3']],
+#                       ['URL2', 'job_title2', ['comp1', 'comp2', 'comp3']],
+#                       ['URL3', ..., [...]]] and so forth.
+print("count('http'): ", jdes.count('http'))
+job_data = [[None, None, []]
+            for i in range(jdes.count('http'))]  # count number of URLs in 'jdes'
+new_words = []
+
+# job_data[0][0] = 'URL'
+# job_data[0][1] = 'job_title'
+
+
 # all this could run with a .. decorator(?) to ensure that the progress is
 # saved, even if it is interrupted.
 
@@ -40,7 +53,7 @@ print(usual_words[:20])
 for word in jdes:
     if word in usual_words:
         print(f"'{word}' in usual_words!")
-    if word not in usual_words:  # and len(word) > 1:
+    if (word not in usual_words) and (word not in new_words):  # and len(word) > 1:
         if word in competences:
             # add to competence list
             print(f"'{word}' should be added to list.")
@@ -53,12 +66,14 @@ for word in jdes:
                 answer = input(f"is '{word}' a competence? [y/n]: ")
                 if answer == "y":
                     competences.append(word)
+                    job_data[0][2].append(word)
                     print(f"'{word}' added to competences.")
-                    print(f"'{word}' should also be added to list.")
+                    print(f"'{word}' added to job_data.")
                     sorted = True
                 elif answer == "n":
-                    usual_words.append(word)
-                    print(f"'{word}' added to usual_words.")
+                    new_words.append(word)
+                    print(f"'{word}' added to new_words.")
                     sorted = True
                 else:
                     print("Please choose 'y' or 'n'.")
+    print("new_words: ", new_words)
