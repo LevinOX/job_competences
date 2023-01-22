00a6fcadc2b115ae30365adcab37abd2466e5261
LevinOX <daniel.wrede@posteo.de>
2023-01-22 19:19:03 +0100

    solve issue with list usual_words

--- filter_job_competences.py
+++ filter_job_competences.py
@@ -15,11 +15,11 @@ def filter_string(string):
     return string
 
 
-with open("job_descriptions.txt", "r") as f:
-    jdescription = filter_string(f.read())
+with open("job_description.txt", "r", encoding='utf-8') as f:
+    jdescription = filter_string(f.read(500))
     jdes = tuple(jdescription.split(sep=' '))
-with open("usual_words_de.csv", "r") as g:
-    usual_words = list(csv.reader(g, delimiter=','))
+with open("usual_words_de_big.csv", "r") as g:
+    usual_words = list(csv.reader(g, delimiter=','))[0]
     # usual_words = g.read()  # <-- list
 with open("competences.csv", "r") as h:
     # str_comp = h.read()
@@ -27,7 +27,7 @@ with open("competences.csv", "r") as h:
 
 print(f"list of competences: \n{competences}")
 print(f"job description: {jdescription}")
-
+print(usual_words[:20])
 # all this could run with a .. decorator(?) to ensure that the progress is
 # saved, even if it is interrupted.
 
@@ -36,8 +36,11 @@ print(f"job description: {jdescription}")
 # 2. if word not known, ask if it is a competence or not, add it to list
 #    'usual_words' or 'competences' and, in latter case, add to list to
 #    describe the job.
+
 for word in jdes:
-    if word not in usual_words and len(word) > 1:
+    if word in usual_words:
+        print(f"'{word}' in usual_words!")
+    if word not in usual_words:  # and len(word) > 1:
         if word in competences:
             # add to competence list
             print(f"'{word}' should be added to list.")
@@ -47,7 +50,7 @@ for word in jdes:
             # or to usual words.
             sorted = False
             while not sorted:
-                answer = input(f"is {word} a competence? [y/n]: ")
+                answer = input(f"is '{word}' a competence? [y/n]: ")
                 if answer == "y":
                     competences.append(word)
                     print(f"'{word}' added to competences.")
