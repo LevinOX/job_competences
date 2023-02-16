9a9e53a4e6969f93842bdd8ce7e93aa09b6244ea
LevinOX <daniel.wrede@posteo.de>
2023-02-16 16:26:53 +0100

    writing to file

--- scrape_with_selenium.py
+++ scrape_with_selenium.py
@@ -1,4 +1,5 @@
 import time
+import re
 
 from selenium import webdriver
 from selenium.webdriver.support import expected_conditions as EC
@@ -24,12 +25,23 @@ chrome_driver = webdriver.Chrome(
 )
 
 
+def write_content(url, data):
+    """write job data to file"""
+    f = open("job_descriptions_arbeitsagentur.txt",
+             "a", encoding='utf-8')
+    f.writelines((url + '\n',
+                  f"{data[0]}, {data[1]} \n",
+                  data[3] + '\n',
+                  data[5] + '\n'))
+    f.close()
+
+
 def arbeitsagentur_scraper():
     URL = "https://www.arbeitsagentur.de/jobsuche/suche?angebotsart=1&was=aws%20python"
     with chrome_driver as driver:
-        driver.implicitly_wait(15)
+        driver.implicitly_wait(5)
         driver.get(URL)
-        wait = WebDriverWait(driver, 10)
+        wait = WebDriverWait(driver, 5)
 
         # wait until this element is visible
         wait.until(EC.visibility_of_element_located(
@@ -46,30 +58,49 @@ def arbeitsagentur_scraper():
             url = elem.get_attribute('href')
             urls.append(url)
 
+        # clean for known urls
+        with open(known_URLs.txt, 'r') as known:
+            known_URLs = list(known.readlines())
+
         for i, url in enumerate(urls):
             print("i: ", i)
             print(f"{url}")
 
-            driver.implicitly_wait(15)
+            driver.implicitly_wait(5)
             driver.get(url)
             # wait.until(EC.visibility_of_element_located(
             #     (By.CSS_SELECTOR, '.liste-container')))
 
             # gather information from page
             try:
-                element = wait.until(EC.visibility_of_element_located(
-                    (By.ID, "jobdetails-beschreibung")))
+                IDs = ("jobdetails-hauptberuf",
+                       "jobdetails-titel",
+                       "jobdetails-veroeffentlichungsdatum",
+                       "jobdetails-kopf-arbeitgeber",
+                       "jobdetails-arbeitsort",
+                       "jobdetails-beschreibung")
+                data = [None]*len(IDs)
+                for j, id in enumerate(IDs):
+                    # print(f"searching {id}")
+                    data[j] = wait.until(EC.visibility_of_element_located(
+                        (By.ID, id))).text
+
                 # element = driver.find_element(
                 #     By.ID, 'jobdetails-beschreibung')
-                print(element.text)
+                print("url: ", url)
+                for j in range(5):
+                    print(f"{IDs[j]}: {data[j]}")
 
+                # print("X: ", data[-1])
+
+                write_content(url, data)
                 # res = driver.page_source  # requests.get(URL)
                 # soup = BeautifulSoup(res, 'html.parser')
                 # print(soup.prettify())
             except TimeoutException:
                 print("Element not found on the page")
-            if i > 0:
-                break
+            # if i > 0:
+            #     break
 
 
 arbeitsagentur_scraper()
